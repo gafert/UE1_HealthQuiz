90add70d85b2efc1982e849596f28dda2e1390ff
gafert <m.gafert@outlook.com>
2017-04-07 21:42:36 +0200

    Implemented cheat activity layout

---
+++ app/src/main/java/fhtw/bsa2/gafert_steiner/ue1_healthquiz/CheatActivity.java
@@ -0,0 +1,15 @@
+package fhtw.bsa2.gafert_steiner.ue1_healthquiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CheatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cheat);
+
+        //TODO: Everything from Point 7
+    }
+}
