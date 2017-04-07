62b7a9ce6a40039c149fde90ee953bd23a86ea1e
gafert <m.gafert@outlook.com>
2017-04-07 19:01:16 +0200

    Corrected Activity

--- app/src/main/java/fhtw/bsa2/gafert_steiner/ue1_healthquiz/QuizActivity.java
+++ app/src/main/java/fhtw/bsa2/gafert_steiner/ue1_healthquiz/QuizActivity.java
@@ -2,8 +2,12 @@ package fhtw.bsa2.gafert_steiner.ue1_healthquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class QuizActivity extends AppCompatActivity {
+
+    private Button mTrueButton;
+    private Button mFalseButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
